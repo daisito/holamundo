768477202ed4a044bd43e4b2713360ee81162321
Daniel Carmona <dcarmona808@gmail.com>
2020-09-01 23:22:14 -0400

    Geolocation Obtener la ubicacion del dispositivo

--- App.js
+++ App.js
@@ -1,32 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, View, Text, Button, Alert } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, Alert } from "react-native";
+import * as Location from  'expo-location';
+import * as Constants from  'expo-constants';
 
 //API DATOS https://jsonplaceholder.typicode.com/users
 // API IMAGES https://placekitten.com/
 export default function App() {
-  const crearDialogo = ()=> Alert.alert(
-    'Titulo',
-    'Cuerpo Mensaje',
-    [
-      {
-        text: 'Cancelar',
-        onPress: ()=>{},
-        style: "cancel", 
-      },
-      {
-        text: 'Aceptar',
-        onPress: ()=> console.log('Boton Presionado'),
-      }
-    ],
-    {
-      cancelable: false,
-    },
-    )
+  
+  const buscaLocation = async ()=>{
+    const {status} = await Location.requestPermissionsAsync()
+    if(status !== 'granted'){
+      return Alert.alert('No tenemos los permisos necesarios para acceder a la locacion')
+    }
+    const location = await Location.getCurrentPositionAsync({})
+    console.log(location);
+  }
 
+  useEffect(()=>{
+    buscaLocation()
+  })
   return (
     <View style={styles.container}>
-      <Button title="Abrir Dialogo" onPress={crearDialogo}></Button>
+      
     </View>
   );
 }
