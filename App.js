de2799385c04716d9ed4ed52b69c994418bb71c5
Daniel Carmona <dcarmona808@gmail.com>
2020-09-01 23:34:25 -0400

    Agregando Mapas y Marcadores

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, Text, Alert } from "react-native";
+import { StyleSheet, View, Text, Alert, Dimensions } from "react-native";
+import MapView, {Marker} from 'react-native-maps';
 import * as Location from  'expo-location';
 import * as Constants from  'expo-constants';
 
 //API DATOS https://jsonplaceholder.typicode.com/users
 // API IMAGES https://placekitten.com/
 export default function App() {
-  
+  const [locacion, setLocacion] = useState({})
   const buscaLocation = async ()=>{
     const {status} = await Location.requestPermissionsAsync()
     if(status !== 'granted'){
       return Alert.alert('No tenemos los permisos necesarios para acceder a la locacion')
     }
     const location = await Location.getCurrentPositionAsync({})
-    console.log(location);
+    setLocacion(location)
   }
 
   useEffect(()=>{
@@ -22,13 +23,24 @@ export default function App() {
   })
   return (
     <View style={styles.container}>
-      
+      <MapView style={styles.map}>
+        {locacion.coords
+          ? <Marker
+          coordinate={locacion.coords}
+          title="Titulo"
+          description="Descripcion del punto" />
+          :null
+        }
+      </MapView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  map:{
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
