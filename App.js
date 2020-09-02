214146a709f7aec69aafe86046df5d66e8579709
Daniel Carmona <dcarmona808@gmail.com>
2020-09-01 23:00:10 -0400

    Alert con varios botones

--- App.js
+++ App.js
@@ -1,46 +1,38 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, View, Modal, Text, Button } from "react-native";
+import { StyleSheet, View, Text, Button, Alert } from "react-native";
 
 //API DATOS https://jsonplaceholder.typicode.com/users
 // API IMAGES https://placekitten.com/
 export default function App() {
-  const [modal, setModal] = useState(false);
+  const crearDialogo = ()=> Alert.alert(
+    'Titulo',
+    'Cuerpo Mensaje',
+    [
+      {
+        text: 'Cancelar',
+        onPress: ()=>{},
+        style: "cancel", 
+      },
+      {
+        text: 'Aceptar',
+        onPress: ()=> console.log('Boton Presionado'),
+      }
+    ],
+    {
+      cancelable: false,
+    },
+    )
 
   return (
     <View style={styles.container}>
-      <Modal animationType="slide" transparent={true} visible={modal}>
-        <View style={styles.center}>
-          <View style={styles.content}>
-            <Text>Soy Modal</Text>
-            <Button title="Cerrar Modal" onPress={()=> setModal(!modal)}></Button>
-          </View>
-        </View>
-      </Modal>
-      <Text>No soy el Modal</Text>
-      <Text>No soy el Modal</Text>
-      <Text>No soy el Modal</Text>
-      <Text>No soy el Modal</Text>
-      <Text>No soy el Modal</Text>
-      <Button title="Abrir Modal" onPress={()=> setModal(!modal)}></Button>
+      <Button title="Abrir Dialogo" onPress={crearDialogo}></Button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  content:{
-    backgroundColor: '#eee',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent : 'center',
-    margin: 25,
-  },
-  center:{
-    flex: 1,
-    alignItems: 'stretch',
-    justifyContent : 'center',
-    backgroundColor: 'rgba(0,0,0,0.3)',
-  },
+  
   container: {
     flex: 1,
     backgroundColor: "#fff",
